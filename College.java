7ecefa0a3531dd9a6de0e98b09b0b407d178b6c0
keerthimanoharan <100154014+keerthimanoharan@users.noreply.github.com>
2022-03-19 22:10:02 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,12 @@
+package org.college;
+
+public class College {
+public void collegeName() {
+	System.out.println("college name is: sona");
+}
+public void collegeCode() {
+	System.out.println("college code is: 1003");
+}
+public void collegeRank() {
+	System.out.println("college rank is: 5");
+}}
