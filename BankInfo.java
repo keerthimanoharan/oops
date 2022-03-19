0df6c14e226c5b5b5e78530f0cfe0fe6492b4a05
keerthimanoharan <100154014+keerthimanoharan@users.noreply.github.com>
2022-03-19 22:26:09 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,13 @@
+package org.college;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("Bank saving");
+	}
+public void fixed() {
+	System.out.println("fixed deposit amount");
+}
+public void deposit() {
+	System.out.println("deposit amount");
+}}
